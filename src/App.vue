06fcaec29847b273e412b65695a3006d23837f19
oscarf4 <oscarvedo97@gmail.com>
2022-03-08 15:24:51 -0500

    Creacion de proyecto Vue2 con instalación y configuración de Bootstrap Vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderBox />
+    <QuestionBox />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderBox from './components/HeaderBox.vue'
+import QuestionBox from './components/QuestionBox.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderBox,
+    QuestionBox
   }
 }
 </script>
